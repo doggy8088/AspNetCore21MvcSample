4d53b4ed8d345c179f9db54b46afff2837c12399
Will <doggy.huang@gmail.com>
2018-08-26 10:16:49 +0800

    01 練習 app.Use 與 app.Run 的 Middleware 基本用法

--- AspNetCore21Mvc/Startup.cs
+++ AspNetCore21Mvc/Startup.cs
@@ -38,25 +38,23 @@ namespace AspNetCore21Mvc
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                app.UseHsts();
-            }
+            app.Use(async (context, next) => {
+                await context.Response.WriteAsync("Hello");
+                await next();
+                await context.Response.WriteAsync("Will");
+            });
 
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-            app.UseCookiePolicy();
+            app.Use(async (context, next) => {
+                await context.Response.WriteAsync("Test");
+                if (context.Request.QueryString.Value != "")
+                {
+                    await next();
+                }
+                await context.Response.WriteAsync("123");
+            });
 
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+            app.Run(async (context) => {
+                await context.Response.WriteAsync("World");
             });
         }
     }
