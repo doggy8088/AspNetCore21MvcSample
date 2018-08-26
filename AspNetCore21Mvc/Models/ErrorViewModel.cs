fb75b90653203ec4dc5eced1f8ea5d0b5c4c6f6b
Will <doggy.huang@gmail.com>
2018-08-26 10:15:59 +0800

    加入專案檔案。

---
+++ AspNetCore21Mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AspNetCore21Mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
